48beebc5c7ca2f6bb3be231566ddee7cd2b3c978
darimuhittin <muhittin.dari@simdatix.com>
2022-09-28 21:22:34 +0300

    init

---
+++ src/hooks/useOutsideAlerter.ts
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+interface ValidRefTarget {
+  contains(target: EventTarget | null): any;
+}
+
+function useOutsideAlerter(ref: React.RefObject<ValidRefTarget>) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        alert("You clicked outside of me!");
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+export default useOutsideAlerter;
